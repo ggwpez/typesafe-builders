cb6e5b0e3726830a2ab1d4487af3f6f0f4c0cd36
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-04-02 16:44:40 +0200

    Cleanup
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- typesafe-builders-core/src/lib.rs
+++ typesafe-builders-core/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(non_upper_case_globals)]
+
 pub trait BuilderFactory<Builder> {
 	fn builder() -> Builder;
 }
--- typesafe-builders/tests/test.rs
+++ typesafe-builders/tests/test.rs
@@ -3,12 +3,22 @@
 use typesafe_builders::prelude::*;
 
 #[test]
-fn it_works() {
+fn example() {
 	#[derive(Builder)]
-	struct Struct {
-		mandatory: u8,
-		optional: Option<u8>,
+	struct Point {
+		x: u8,
+		y: u8,
 	}
 
-	let builder = Struct::builder();
+	let builder = Point::builder();
+	let partial = builder.with_x(5);
+	// These do not compile:
+	// partial.with_x(8);
+	// partial.build();
+
+	// Infallibly construct an instance:
+	let result = partial.with_y(8).build();
+
+	assert_eq!(result.x, 5);
+	assert_eq!(result.y, 8);
 }
