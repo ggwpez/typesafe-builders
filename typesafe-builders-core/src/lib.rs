ed1c5ace7f8b7cc706d73429d4661461a1ce2317
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-04-04 14:18:42 +0200

    Implement #[optional]
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- typesafe-builders-core/src/lib.rs
+++ typesafe-builders-core/src/lib.rs
@@ -1,11 +1,9 @@
 #![allow(non_upper_case_globals)]
 
-extern crate proc_macro;
-use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use syn::spanned::Spanned;
 
-pub struct FieldAttrs(pub std::collections::HashMap<FieldAttrId, FieldAttrVal>);
+pub type FieldAttrs = std::collections::HashMap<FieldAttrId, FieldAttrVal>;
 
 #[derive(Debug)]
 pub struct FieldAttr {
@@ -13,12 +11,12 @@ pub struct FieldAttr {
 	pub val: FieldAttrVal,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 pub enum FieldAttrId {
 	Optional,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 pub enum FieldAttrVal {
 	/// Inherent the default value. This is the default.
 	Inherit,
@@ -31,7 +29,7 @@ pub trait BuilderFactory<Builder> {
 	fn builder() -> Builder;
 }
 
-pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::Error> {
+pub fn impl_derive_builder(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
 	let syn::Data::Struct(ref s) = ast.data else {
 		return Err(syn::Error::new_spanned(
 			ast,
@@ -46,15 +44,27 @@ pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::E
 	let (mut builder_field_names, mut builder_field_types, mut builder_fields) =
 		(vec![], vec![], vec![]);
 	let mut builder_const_generics = vec![];
-	let mut builder_const_generics_all_false = vec![];
-	let mut builder_const_generics_all_true = vec![];
+	let mut builder_const_generics_all_unset = vec![];
+	let mut builder_const_generics_all_set = vec![];
+
+	// The dont-cares for optional fields:
+	let mut builder_optional_generics = vec![];
+	let mut builder_optional_generic_values = vec![];
 
 	let mut setters = vec![];
 	for (i, field) in s.fields.iter().enumerate() {
-		let mut field_attrs = vec![];
-		for attr in field.attrs.iter() {
-			let attr: FieldAttr = attr.try_into()?;
-			field_attrs.push(attr);
+		let field_attrs = extract_attributes(&field)?;
+		let optional = is_optional(field, &field_attrs);
+
+		if optional {
+			let field_name = syn::Ident::new(
+				&format!("{}_set", field.ident.clone().unwrap()).to_uppercase(),
+				field.ident.span(),
+			);
+			builder_optional_generics.push(quote! { const #field_name: bool });
+			builder_optional_generic_values.push(quote! { #field_name });
+		} else {
+			builder_optional_generic_values.push(quote! { true });
 		}
 
 		let field_name = field.ident.clone().unwrap();
@@ -62,10 +72,11 @@ pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::E
 		builder_field_names.push(field_name.clone());
 		builder_field_types.push(field_type.clone());
 		builder_fields.push(quote! { #field_name: Option<#field_type> });
-		let const_generic_name = syn::Ident::new(&format!("{}_set", field_name), field_name.span());
+		let const_generic_name =
+			syn::Ident::new(&format!("{}_set", field_name).to_uppercase(), field_name.span());
 		builder_const_generics.push(quote! { const #const_generic_name: bool });
-		builder_const_generics_all_false.push(quote! { false });
-		builder_const_generics_all_true.push(quote! { true });
+		builder_const_generics_all_unset.push(quote! { false });
+		builder_const_generics_all_set.push(quote! { true });
 
 		let setter_name = syn::Ident::new(&format!("{}", field_name), field_name.span());
 		let mut const_generics = vec![];
@@ -103,11 +114,22 @@ pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::E
         });
 	}
 	let build_fn = quote! {
-		impl #builder_ident<#(#builder_const_generics_all_true),*> {
+		// The `build` that requires all to be set. This one is always implemented.
+		/*impl #builder_ident<#(#builder_const_generics_all_set),*> {
 			#[allow(dead_code)]
 			pub fn build(self) -> #name {
 				#name {
 					#(#builder_field_names: self.#builder_field_names.unwrap()),*
+				}
+			}
+		}*/
+
+		// The `build` that is available as soon as all mandatory fields are set.
+		impl<#(#builder_optional_generics),*> #builder_ident<#(#builder_optional_generic_values),*> {
+			#[allow(dead_code)]
+			pub fn build(self) -> #name {
+				#name {
+					#(#builder_field_names: self.#builder_field_names.unwrap_or_default()),*
 				}
 			}
 		}
@@ -129,9 +151,9 @@ pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::E
 	};
 
 	let gen = quote! {
-		impl ::typesafe_builders::prelude::BuilderFactory<#builder_ident<#(#builder_const_generics_all_false),*>> for #name {
+		impl ::typesafe_builders::prelude::BuilderFactory<#builder_ident<#(#builder_const_generics_all_unset),*>> for #name {
 			#[allow(dead_code)]
-			fn builder() -> #builder_ident<#(#builder_const_generics_all_false),*> {
+			fn builder() -> #builder_ident<#(#builder_const_generics_all_unset),*> {
 				#builder_ident {
 					#(#builder_field_names: None),*
 				}
@@ -144,6 +166,27 @@ pub fn impl_derive_builder(ast: &syn::DeriveInput) -> Result<TokenStream, syn::E
 		#gen
 	}
 	.into())
+}
+
+fn extract_attributes(field: &syn::Field) -> Result<FieldAttrs, syn::Error> {
+	let mut field_attrs = FieldAttrs::new();
+
+	for attr in field.attrs.iter() {
+		let fattr: FieldAttr = attr.try_into()?;
+		if field_attrs.contains_key(&fattr.id) {
+			return Err(syn::Error::new_spanned(attr, "Duplicate attribute on struct field"))
+		}
+		field_attrs.insert(fattr.id, fattr.val);
+	}
+
+	Ok(field_attrs)
+}
+
+fn is_optional(_field: &syn::Field, attrs: &FieldAttrs) -> bool {
+	match attrs.get(&FieldAttrId::Optional).unwrap_or(&FieldAttrVal::Inherit) {
+		FieldAttrVal::Override(o) => *o,
+		FieldAttrVal::Inherit => false,
+	}
 }
 
 impl TryFrom<&syn::Attribute> for FieldAttr {
--- typesafe-builders/tests/test.rs
+++ typesafe-builders/tests/test.rs
@@ -5,21 +5,23 @@ use typesafe_builders::prelude::*;
 fn example() {
 	#[derive(Builder)]
 	struct Point {
-		#[optional]
 		x: u8,
 		y: u8,
+		#[optional]
+		z: Option<u8>,
 	}
 
 	let builder = Point::builder();
 	let partial = builder.x(5);
 	// These do not compile:
-	// partial.x(8);
-	// partial.build();
+	// partial.x(6); 		// `x` is already set
+	// partial.build();		// `y` is not set
 
-	// Set all fields to enable the `build` function:
+	// Set all required fields to enable the `build` function:
 	let complete = partial.y(8);
 	let result = complete.build();
 
 	assert_eq!(result.x, 5);
 	assert_eq!(result.y, 8);
+	assert_eq!(result.z, None);
 }
